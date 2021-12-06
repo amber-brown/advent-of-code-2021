8f9fa53818d23179c75d7b1fd018f063cb3c2221
Amber Brown <amberbrown07@hotmail.co.uk>
2021-12-06 08:22:28 +0000

    Day 05

--- 05/05.js
+++ 05/05.js
@@ -7,32 +7,73 @@ const horizontalAndVerticalLines = coords.filter(x => {
     return x[0][0] === x[1][0] || x[0][1] === x[1][1]
 })
 
-const coveredAreas = horizontalAndVerticalLines.map(x => {
-    if(x[0][0] === x[1][0]) {
-        const greatest = Math.max(x[0][1], x[1][1]) 
-        let i = Math.min(x[0][1], x[1][1]) + 1
-        let newVals = []
-        while (i < greatest) {
-            newVals.push([x[0][0], i])
-            i++
-        }
-        return [...x, ...newVals]
+const getPointsOnHorizontalLine = coord => {
+    const [[x1, y1], [x2, y2]] = coord 
+    const greatest = Math.max(y1, y2) 
+    let i = Math.min(y1, y2) + 1
+    let newVals = []
+    while (i < greatest) {
+        newVals.push([x1, i])
+        i++
+    }
+    return [...coord, ...newVals]
+}
+
+const getPointsOnVerticalLine = coord => {
+    const [[x1, y1], [x2, y2]] = coord 
+    const greatest = Math.max(x1, x2) 
+    let i = Math.min(x1, x2) + 1
+    let newVals = []
+    while (i < greatest) {
+        newVals.push([i, y1])
+        i++
+    }
+    return [...coord, ...newVals]
+}
+
+const getPointsOnDiagonalLine = coord => {
+    const [[x1, y1], [x2, y2]] = coord 
+    let pointsCrossed = []
+
+    const stepX = x1 < x2 ? 1 : -1;
+    const stepY = y1 < y2 ? 1 : -1;
+    for (let i = 0; i <= Math.abs(x1 - x2); i++) {
+        const x = x1 + i * stepX;
+        const y = y1 + i * stepY;
+        pointsCrossed.push([x, y]);
+    }
+    return pointsCrossed
+}
+
+const countPointsWithIntersection = coords => {
+    const counts = {};
+    coords.forEach(x => { 
+        counts[x] = (counts[x] || 0) + 1; 
+    });
+
+    return Object.values(counts).filter(x => x > 1).length
+}
+
+const coveredAreas = horizontalAndVerticalLines.map(coord => {
+    const [[x1, y1], [x2, y2]] = coord
+    if(x1 === x2) {
+        return getPointsOnHorizontalLine(coord)
     } else {
-        const greatest = Math.max(x[0][0], x[1][0]) 
-        let i = Math.min(x[0][0], x[1][0]) + 1
-        let newVals = []
-        while (i < greatest) {
-            newVals.push([i, x[0][1]])
-            i++
-        }
-        return [...x, ...newVals]
+        return getPointsOnVerticalLine(coord)
     }
 }).flat();
 
-const counts = {};
-coveredAreas.forEach(x => { 
-    counts[x] = (counts[x] || 0) + 1; 
-});
+console.log('Answer 1:', countPointsWithIntersection(coveredAreas))
 
-const answer1 = Object.values(counts).filter(x => x > 1).length
-console.log(answer1)
+const everyCoveredPoint = coords.map(coord => {
+    const [[x1, y1], [x2, y2]] = coord
+    if(x1 === x2) {
+        return getPointsOnHorizontalLine(coord)
+    } else if (y1 === y2) {
+        return getPointsOnVerticalLine(coord)
+    } else {
+        return getPointsOnDiagonalLine(coord)
+    }
+}).flat()
+
+console.log('Answer 2:', countPointsWithIntersection(everyCoveredPoint))
