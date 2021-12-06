889fa73f6b5271ab9afab00295dca5fa07311ec3
Amber Brown <amberbrown07@hotmail.co.uk>
2021-12-06 12:56:57 +0000

    Day 06

--- 06/06.js
+++ 06/06.js
@@ -3,25 +3,27 @@ const input = fs.readFileSync('./input.txt').toString();
 
 const laternFish = input.split(',').map(x => parseInt(x));
 
-const getFishCountAfterDays = (fish, days) => {
-    let currentFish = [...fish]
-    
-    for(let i = 0; i < days; i++) {
-        let newFishCount = 0
-        currentFish.forEach((f, i) => {
-            if(f === 0) {
-                currentFish[i] = 6
-                newFishCount++
-            } else {
-                currentFish[i] = f-1
-            }
-        })
+const getFishCountAfterDays = (initialFish, days) => {
+    let daysUntilReproductionFishCount = new Array(9).fill(0);
+    initialFish.forEach((time) => daysUntilReproductionFishCount[time]++);
 
-        currentFish = [...currentFish, ...new Array(newFishCount).fill(8)]
+    for(let i = 0; i < days; i++) {
+        // remove all fish that reproduce from day 1 
+        //(moves all other items into the new day position in the array)
+        const fishThatReproducedCount = daysUntilReproductionFishCount.shift()
+
+        //add babies to end of array
+        daysUntilReproductionFishCount.push(fishThatReproducedCount)
+
+        //fish that reproduced then reset back to day 6
+        daysUntilReproductionFishCount[6] += fishThatReproducedCount;
     }
 
-    return currentFish.length
+    return daysUntilReproductionFishCount.reduce((acc, val) => acc + val, 0);
 }
 
-console.log('Answer 1:', getFishCountAfterDays(laternFish, 80))
+console.log('Answer 1:', getFishCountAfterDays(laternFish, 80)) //365131
+
+console.log('Answer 2', getFishCountAfterDays(laternFish, 256)) // 1650309278600
+
 
